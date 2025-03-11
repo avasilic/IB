3e6782114b9065debeb685a8552dfa654757651f
net-tech- <hello@nettech.dev>
2025-03-11 14:02:43 +0100

    doing it in the same line

--- firstProject/Lesson2.java
+++ firstProject/Lesson2.java
@@ -9,10 +9,15 @@ public class Lesson2 {
 	public static void main(String args[]) {
 		int number; // This declares an integer variable
 		number = 100; // The variable is not initialized and 100 is cur. val.
+		// This is also a assign command
+		// We can do init. and decl. in the same line int number = 10;
 
 		System.out.println("number"); // This will output the word "number", not
 		// the value of the variable
 		System.out.println(number); // This will print the current value of the
 		// variable number
+		
+		// int number = 10; Not possible because number is declared
+		
 	}
 }
