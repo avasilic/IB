d2f5ec4f55d1622593894ab3e337ead32df1856b
net-tech- <hello@nettech.dev>
2025-03-11 13:55:02 +0100

    init a number

--- firstProject/Lesson2.java
+++ firstProject/Lesson2.java
@@ -7,6 +7,10 @@
 */
 public class Lesson2 {
     public static void main(String args[]) {
-
+    	int number; // This declares an integer variable
+     	number = 100; // The variable is not initialized and 100 is cur. val.
+     
+      	System.out.println("number");
+       System.out.println(number);
     }
 }
