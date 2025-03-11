37fe6e20993438918972f0b20585ae5b78c333cb
net-tech- <hello@nettech.dev>
2025-03-11 13:13:08 +0100

    add comments

--- firstProject/Lesson1.java
+++ firstProject/Lesson1.java
@@ -1,7 +1,7 @@
 public class Lesson1 {
     public static void main(String args[]) {
-        System.out.println("Hello world!");
-        System.out.print("Welcome to the ");
+        System.out.println("Hello world!"); // Print (new line)
+        System.out.print("Welcome to the "); // Print (same line)
         System.out.print("CS class");
     }
 }
