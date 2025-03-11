7fdbedfe4b2f2e88321e434ca98f997a79f7c8d8
net-tech- <hello@nettech.dev>
2025-03-11 13:45:02 +0100

    init lesson2

---
+++ firstProject/Lesson2.java
@@ -0,0 +1,12 @@
+/*
+ * Author: Aleksandar Vasilić
+ * 2025
+ * Variables, constants, naming conventions, declaring, initializing, and
+ * assign command.
+ ************************************************
+*/
+public class Lesson2 {
+	public static void main(String args[]) {
+
+	}
+}
