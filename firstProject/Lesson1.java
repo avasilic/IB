8d8439bac64be429df88ed8c26699fd58153cced
net-tech- <hello@nettech.dev>
2025-03-11 13:17:48 +0100

    more detail

--- firstProject/Lesson1.java
+++ firstProject/Lesson1.java
@@ -2,6 +2,6 @@ public class Lesson1 {
 	public static void main(String args[]) {
 		System.out.println("Hello world!"); // Print content & new line
 		System.out.print("Welcome to the ");
-		System.out.print("CS class"); // Print content
+		System.out.print("CS class"); // Print content on the same line
 	}
 }
