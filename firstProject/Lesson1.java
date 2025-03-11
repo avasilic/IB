783b82fcecb977b359a8e897bc9cbea62bc81cb9
net-tech- <hello@nettech.dev>
2025-03-11 13:05:00 +0100

    welcome to the cs class

--- firstProject/Lesson1.java
+++ firstProject/Lesson1.java
@@ -1,5 +1,7 @@
 public class Lesson1 {
     public static void main(String args[]) {
         System.out.println("Hello world!");
+        System.out.print("Welcome to the ");
+        System.out.print("CS class");
     }
 }
