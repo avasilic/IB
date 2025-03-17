1d9431b6788a2958ef3b17bd0d4ff89014a8ba62
net-tech- <hello@nettech.dev>
2025-03-17 23:31:55 +0100

    2025-03-18 homework

---
+++ homework-2025-03-18/Lesson2Exercise.java
@@ -0,0 +1,25 @@
+/**
+ * @author Aleksandar Vasilić
+ * @version 2025-03-17
+ */
+public class Lesson2Exercise {
+	public static void main(String args[]) {
+		/**
+		 * 1. Strings, ints, and doubles
+		 * 2. A string variable
+		 * 3. A double since it is a decimal value
+		 * 4. An int because it is a whole number
+		 * 5. double p = 1.921e-16;
+		 * 6. int i = 407;
+		 * 7. String my_name = "Aleksandar Vasilić";
+		 * 8. int count;
+		 * 9. bankBalance = 136.05;
+		 * 10. scooter13, 139_scooter, ab, c, doubled
+		 * 11. groovyDude, groovy_dude. There are others that are syntactically
+		 * correct but these are encouraged.
+		 * 12. The first line is legal and assigns an integer to a double. The
+		 * second one is illegal as we you are trying to assign a decimal value
+		 * to an integer which only accepts whole numbers.
+		 */
+	}
+}
---
+++ homework-2025-03-18/Tester.java
@@ -0,0 +1,13 @@
+/**
+ * @author Aleksandar Vasilić
+ * @version 2025-03-17
+ */
+public class Tester {
+	public static void main(String args[]) {
+		System.out.println("From: Bill Smith");
+		System.out.println("Address: Dell Computer, Bldg 13");
+		System.out.println("Date: April 12, 2005");
+		System.out.println("To: Jack Jones");
+		System.out.println("Message: Help! I'm trapped inside a computer!");
+	}
+}
