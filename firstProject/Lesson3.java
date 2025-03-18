b319047ea19bebec44a52551107ab6c572fbd219
net-tech- <hello@nettech.dev>
2025-03-18 13:24:55 +0100

    uppercase and lowercase

--- firstProject/Lesson3.java
+++ firstProject/Lesson3.java
@@ -13,10 +13,18 @@ public class Lesson3 {
 		System.out.println(first + second); // No blank in between Happy and
 		// New Year
 		System.out.println(first + " " + second);
-		
+
 		// String methods
-		 
+
 		System.out.println(first.length()); // Length is a method so we use ()
 		// to call it. It returns the number of characters in the string
+
+		System.out.println(first.toUpperCase()); // Makes all characters
+		// uppercase
+		System.out.println(first); // This shows that these methods do not
+		// mutate the string
+
+		second = second.toLowerCase(); // The value of second is changed here
+		System.out.println(second);
 	}
 }
