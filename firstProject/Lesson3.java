c58bf6537c39bbe753b693e8f2199349a156bd9b
net-tech- <hello@nettech.dev>
2025-03-18 13:47:46 +0100

    polymorphism

--- firstProject/Lesson3.java
+++ firstProject/Lesson3.java
@@ -26,18 +26,21 @@ public class Lesson3 {
 
 		second = second.toLowerCase(); // The value of second is changed here
 		System.out.println(second);
-		
+
 		System.out.println(second.substring(2)); // In Java, strings are
-	 	// zero-indexed. Calling substring(2) will, from that position on, 
-	 	// return the substring starting from the third character.
+		// zero-indexed. Calling substring(2) will, from that position on,
+		// return the substring starting from the third character.
 		// Zero-indexed means that counting starts from 0. So, n = 0, e = 1, ...
 
 		System.out.println(second.substring(2, 6)); // Returns the substring
-	 	// from the third character to the fifth character. The first one is
+		// from the third character to the fifth character. The first one is
 		// inclusive. The second is exclusive.
 		// 2 and 6 are arguments for the method call.
-		
+
 		// System.out.println(second.substring()); This will error as we did not
 		// provide any arguments
+
+		// The existence of two methods with the same name with a different
+		// number of arguments is an example of polymorphism.
 	}
 }
