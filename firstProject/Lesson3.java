f12b8e35fb1e618a78eb0e7f1b7834640b76b339
net-tech- <hello@nettech.dev>
2025-03-18 13:11:51 +0100

    contatenation

--- firstProject/Lesson3.java
+++ firstProject/Lesson3.java
@@ -9,5 +9,9 @@ public class Lesson3 {
 	public static void main(String args[]) {
 		String first = "Happy"; // The value must be written between quotation
 		// marks
+		String second = "New Year";
+		System.out.println(first + second); // No blank in between Happy and
+		// New Year
+		System.out.println(first + " " + second);
 	}
 }
