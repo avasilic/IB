6e6656d7b54f669eddfd8dbd48a74704309e35be
net-tech- <hello@nettech.dev>
2025-03-25 13:27:06 +0100

    format and init

---
+++ firstProject/Lesson5.java
@@ -0,0 +1,13 @@
+/*
+ * Author: Aleksandar Vasilić
+ * Date: 2025-03-25
+ * Use of numerical variables and math operations. Related to Lesson 4
+ * in the Java BluePelican book. 
+ ************************************************
+*/
+
+public class Lesson5 {
+	public static void main(String args[]) {
+		
+	}
+}
