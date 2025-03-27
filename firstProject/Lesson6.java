4149009d1ed270132a7e68aa65297df78bfbc64c
net-tech- <hello@nettech.dev>
2025-03-27 11:03:47 +0100

    Create Lesson6.java

---
+++ firstProject/Lesson6.java
@@ -0,0 +1,12 @@
+/*
+ * Author: Aleksandar Vasilić
+ * Date: 2025-03-28
+ * Swapping of variables.
+ ************************************************
+*/
+
+public class Lesson6 {
+	public static void main(String args[]) {
+
+	}
+}
