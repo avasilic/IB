bb2e9ea563a287d997f88a9a30781cfd95927aa5
net-tech- <hello@nettech.dev>
2025-03-27 11:09:18 +0100

    Add variable swap example using temporary variable

--- firstProject/Lesson6.java
+++ firstProject/Lesson6.java
@@ -7,6 +7,14 @@
 
 public class Lesson6 {
 	public static void main(String args[]) {
+		int h = 17;
+		int q = 5;
 
+		System.out.println("h=" + h + " q=" + q);
+		
+		// Swap the values of the variables
+		int temp = h;
+		h = q;
+		q = temp;
 	}
 }
