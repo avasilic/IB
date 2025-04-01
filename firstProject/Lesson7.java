77610f6b7ee2d96b538e2b77fd16a5c58a08162d
net-tech- <hello@nettech.dev>
2025-04-01 13:25:10 +0200

    Add examples of scientific notation and Math.pow() methods

--- firstProject/Lesson7.java
+++ firstProject/Lesson7.java
@@ -8,6 +8,11 @@
 
 public class Lesson7 {
 	public static void main(String args[]) {
+		double d = 1.23E3; // This is the same as 1.23 * 10^3
+		System.out.println(d);
 
+		System.out.println(Math.pow(2, 4)); // 2^4
+		System.out.println(Math.pow(2.3, 4.8)); // Doesn't matter if its decimal
+		System.out.println(Math.pow(2, -1)); // Doesn't matter if its negative
 	}
 }
