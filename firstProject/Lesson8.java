5632f52691013a502d6365dc13799ad3979850d8
net-tech- <hello@nettech.dev>
2025-04-03 10:49:54 +0200

    Create Lesson8.java

---
+++ firstProject/Lesson8.java
@@ -0,0 +1,12 @@
+/*
+ * Author: Aleksandar Vasilić
+ * Date: 2025-04-03
+ * Input from the keyboar. Related to Lesson 7 in the Java BluePelican book.
+ ************************************************
+*/
+
+public class Lesson8 {
+	public static void main(String args[]) {
+
+	}
+}
