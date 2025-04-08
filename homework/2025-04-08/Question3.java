e9ee34b7177c490f9473c896e6eed4f62fb3e267
net-tech- <hello@nettech.dev>
2025-04-08 13:48:51 +0200

    change things to please the lord damir

--- homework/2025-04-08/Question3.java
+++ homework/2025-04-08/Question3.java
@@ -7,12 +7,12 @@ public class Question3 {
 		// Q3: The user enters the length of the hypotenuse and one leg.
 		// Calculate and print the value of the other leg.
 		System.out.println("Please enter the length of the hypotenuse of a triangle:");
-		Double hypotenuse = kbReader.nextDouble();
+		double hypotenuse = kbReader.nextDouble();
 		System.out.println("Please enter the length of one leg of the triangle:");
-		Double leg = kbReader.nextDouble();
+		double leg = kbReader.nextDouble();
 
 		// Using the formula a^2 + b^2 = c^2, so c = sqrt(a^2 + b^2)
-		Double otherLeg = Math.sqrt((hypotenuse * hypotenuse) - (leg * leg));
+		double otherLeg = Math.sqrt(Math.pow(hypotenuse, 2) - Math.pow(leg, 2));
 
 		System.out.println("The length of the other leg is: " + otherLeg);
 
