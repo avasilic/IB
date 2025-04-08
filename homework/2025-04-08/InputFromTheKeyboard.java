a69db8372f49fb37ec270cf14b321358e6901fc6
net-tech- <hello@nettech.dev>
2025-04-08 13:34:57 +0200

    Add implementation for line equation calculator (Q2)

--- homework/2025-04-08/InputFromTheKeyboard.java
+++ homework/2025-04-08/InputFromTheKeyboard.java
@@ -14,5 +14,14 @@ public class InputFromTheKeyboard {
 		System.out.println("[Q1] Sum: " + (firstInteger + secondInteger + thirdInteger));
 		System.out.println("[Q1] Average: " + ((double) (firstInteger + secondInteger + thirdInteger) / 3));
 
+		// Q2: The equation of the straight line y = kx + n is given by entering
+		// coefficients k and n (real numbers). For an entered x, print the
+		// corresponding y value. Use nextDouble()
+		System.out.println("[Q2] Please enter the coefficients k and n for the equation of the line y=kx+n");
+		Double k = kbReader.nextDouble();
+		Double n = kbReader.nextDouble();
+		System.out.println("[Q2] Please enter the x value for the line:");
+		Double x = kbReader.nextDouble();
+		System.out.println("[Q2] y = " + (k * x + n));
 	}
 }
