4bd2b494e8256c670469b50cf58a68a7eeb1265e
net-tech- <hello@nettech.dev>
2025-04-22 14:10:37 +0200

    aaaa

--- homework/2025-04-22/Question8.java
+++ homework/2025-04-22/Question8.java
@@ -30,27 +30,27 @@ public class Question8 {
 		}
 
 		/*
-		Testing:
-		We need to test each branch.
-		Case 1: a > b, a > c
-		Input: a = 5, b = 3, c = 2
-		Output: The maximum is 5
-		Branches: a > b → a > c
-		
-		Case 2: a > b, a <= c
-		Input: a = 5, b = 3, c = 8
-		Output: The maximum is 8
-		Branches: a > b → a <= c
-		
-		Case 3: a <= b, b > c
-		Input: a = 3, b = 5, c = 2
-		Output: The maximum is 5
-		Branches: a <= b → b > c
-		
-		Case 4: a <= b, b <= c
-		Input: a = 3, b = 5, c = 8
-		Output: The maximum is 8
-		Branches: a <= b → b <= c
-		*/
+		 * Testing:
+		 * We need to test each branch.
+		 * Case 1: a > b, a > c
+		 * Input: a = 5, b = 3, c = 2
+		 * Output: The maximum is 5
+		 * Branches: a > b → a > c
+		 * 
+		 * Case 2: a > b, a <= c
+		 * Input: a = 5, b = 3, c = 8
+		 * Output: The maximum is 8
+		 * Branches: a > b → a <= c
+		 * 
+		 * Case 3: a <= b, b > c
+		 * Input: a = 3, b = 5, c = 2
+		 * Output: The maximum is 5
+		 * Branches: a <= b → b > c
+		 * 
+		 * Case 4: a <= b, b <= c
+		 * Input: a = 3, b = 5, c = 8
+		 * Output: The maximum is 8
+		 * Branches: a <= b → b <= c
+		 */
 	}
 }
