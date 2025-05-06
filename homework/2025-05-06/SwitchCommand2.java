6c673d6251ab8bb48249d02a60b5d06f957632a1
net-tech- <hello@nettech.dev>
2025-05-06 14:09:50 +0200

    handle spaces and lowercase

--- homework/2025-05-06/SwitchCommand2.java
+++ homework/2025-05-06/SwitchCommand2.java
@@ -10,7 +10,7 @@ public class SwitchCommand2 {
 		System.out.print("Your choice is:");
 
 		Scanner kbReader = new Scanner(System.in);
-		String menuInput = kbReader.nextLine();
+		String menuInput = kbReader.nextLine().trim();
 		char choice = menuInput.charAt(0);
 
 		System.out.print("First value:");
@@ -19,18 +19,22 @@ public class SwitchCommand2 {
 		double val2 = kbReader.nextDouble();
 
 		switch (choice) {
+			case 'a': // This will 'fall through' to the next case as there is no 'break' statement
 			case 'A':
 				// Case 1: addition
 				System.out.println(val1 + "+" + val2 + "=" + (val1 + val2));
 				break;
+			case 'b':
 			case 'B':
 				// Case 2: subtraction
 				System.out.println(val1 + "-" + val2 + "=" + (val1 - val2));
 				break;
+			case 'c':
 			case 'C':
 				// Case 3: multiplication
 				System.out.println(val1 + "x" + val2 + "=" + (val1 * val2));
 				break;
+			case 'd':
 			case 'D':
 				// Case 4: division
 				if (val2 == 0) {
