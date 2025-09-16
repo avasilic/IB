aab7645f5ed85ba1cc0d66844c98af4a559fbc4c
Aleksandar <aleksandar@avasilic.com>
2025-09-16 13:16:03 +0200

    do it how the prof wants it to be done

--- secondYear/Lesson14.java
+++ secondYear/Lesson14.java
@@ -2,7 +2,7 @@
 /*
  * Author: Aleksandar Vasilić
  * Date: 2025-09-16
- * Question 14: Create an array of 100 elements such that the first one is 1, second is 2, then 3,4 etc. Print all elements. Use only one for loop
+ * Question 14: Create an array of 100 elements such that the first one is 1, second is 2, then 3,4 etc. Print all elements. Use two for loops
  ************************************************
 */
 public class Lesson14 {
@@ -10,7 +10,10 @@ public class Lesson14 {
 		int[] numbers = new int[100];
 		for (int i = 0; i < numbers.length; i++) {
 			numbers[i] = i + 1;
-			System.out.print(numbers[i] + " ");
+		}
+
+		for (int j = 0; j < numbers.length; j++) {
+			System.out.print(numbers[j] + " ");
 		}
 	}
 }
