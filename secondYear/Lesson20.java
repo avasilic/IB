bbd3f370752a8417da2ef0324514ab7b068282e8
Aleksandar <aleksandar@avasilic.com>
2025-10-03 09:45:39 +0200

    make it fancy

--- secondYear/Lesson20.java
+++ secondYear/Lesson20.java
@@ -9,24 +9,45 @@ public class Lesson20 {
 		int[] integers = { 10, 12, 8, 7, 1, 5, 2, 4 };
 
 		boolean oneMorePassRequired = true;
+		int numberOfPasses = 0;
+		int numberOfComparisons = 0;
+		int numberOfSwitches = 0;
 
 		while (oneMorePassRequired) {
 			oneMorePassRequired = false;
+			numberOfPasses++;
 			for (int i = 0; i < integers.length - 1; i++) {
+				numberOfComparisons++;
 				if (integers[i] > integers[i + 1]) {
 					oneMorePassRequired = true;
+					numberOfSwitches++;
 					int tempInt = integers[i];
 					// Note to self: order is important here
 					integers[i] = integers[i + 1];
 					integers[i + 1] = tempInt;
 				}
 			}
+
+			System.out.print("Pass " + numberOfPasses + ": ");
+			for (int i = 0; i < integers.length; i++) {
+				if (i >= integers.length - numberOfPasses) {
+					System.out.print("[" + integers[i] + "]\t");
+				} else {
+					System.out.print(integers[i] + "\t");
+				}
+			}
+			System.out.println();
 		}
 
 		System.out.println("The sorted array is:");
 
-		for (int i = 0; i < integers.length - 1; i++) {
-			System.out.print(integers[i] + " ");
+		for (int i = 0; i < integers.length; i++) {
+			System.out.print(integers[i] + "\t");
 		}
+
+		System.out.println();
+
+		System.out.println("Number of comparisons: " + numberOfComparisons);
+		System.out.println("Number of switches: " + numberOfSwitches);
 	}
 }
